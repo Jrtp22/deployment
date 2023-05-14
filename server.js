4f0d152edd7cbd958d7a74bb3c46c80f7b3cc43e
jrtp22 <jrtp22@gmail.com>
2023-05-14 11:27:43 -0700

    port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT 
 
 app.get('/', (req, res) => {
     res.send("Hello, world!");
 });
-app.listen(3000);
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+module.exports = app;
